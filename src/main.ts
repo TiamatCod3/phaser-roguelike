496aa9e4c05b43fb36e3c0b092e73e378234bcb7
TiamatCod3 <ti_matos@id.uff.br>
2023-02-24 10:59:50 -0300

    Brincando com as fontes

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,43 @@
-import { FirstGameScene } from './first-scene';
+// import { FirstGameScene } from './first-scene';
+// import { MyScene } from './my-scene';
 import Phaser from 'phaser';
 import './style.css';
 
-const config: Phaser.Types.Core.GameConfig = {
-    width: 800,
-    height: 600,
-    type: Phaser.AUTO,
-    parent: 'game-container',
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 300 },
-        debug: false
-      }
-    },
-    scene: [FirstGameScene]
-  };
 
-new Phaser.Game(config);
+
+const scene:any = {
+  preload: function () {
+    this.load.bitmapFont("arcade", 
+    "src/assets/font/pressstart.png",
+    "src/assets/font/pressstart.fnt"
+    )
+    this.load.bitmapFont("arcade",
+    "src/assets/font/pressstart.png",
+    "src/assets/font/pressstart.fnt"
+    )
+  },
+  create: function () {
+    this.add.bitmapText(400, 300, "arcade", 
+    "Hello Phaser").setOrigin(0.5);
+    this.add.bitmapText(100,100, "arcade",
+        "Bom dia, pessoal").setOrigin(0);
+  }
+
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: 800,
+  height: 600,
+  backgroundColor: "#000",
+  parent: "game",
+  pixelArt: true,
+  scene: scene,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  }
+};
+const game = new Phaser.Game(config);
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/phaser-ts-template/',
+    base: '/phaser-roguelike/',
     server:{
         port:3000
     },
